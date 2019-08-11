6f6eb0b6b3f13321299369566da9a19c7dfff598
Peter <peter.w570@gmail.com>
2019-08-11 15:46:36 +0100

    [main] add comments for config options

--- main.js
+++ main.js
@@ -4,8 +4,12 @@ const parseSchedule = require('./parser');
  * conifg contains:
  * URL - url of the google sheeet
  * TOURNAMENT - name of the tournament
- * DIVISIONS - ability to cinfigure for multiple divisions in a single sheet
+ * DIVISIONS - ability to configure for multiple divisions in a single sheet
  * DAYS_TO_DATES - object with the name of the day as the key and 'YYYY-MM-DD' as the value
+ * DIVISION - if you don't specify DIVISIONS you can say which division it is
+ * SEED_TAB_IDX - (0 based) index
+ * POOL_RES_TAB_IDX - (0 based) index of the pool result tab
+ * BRACKET_RES_TAB_IDX - can be null, a (0 based) index, or an array of (0 based) indices
  * TODO: specify what you want to print out
  */
 const config = require('./config'); // TODO: read from command line args
